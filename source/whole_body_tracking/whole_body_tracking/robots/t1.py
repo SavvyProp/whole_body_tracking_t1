e95b822cb4391d30bcf14dd1441c9f0b39dcf862
SavvyProp <ludwigtaycheeying@gmail.com>
2025-11-01 00:34:04 -0400

    lower gains

--- source/whole_body_tracking/whole_body_tracking/robots/t1.py
+++ source/whole_body_tracking/whole_body_tracking/robots/t1.py
@@ -100,6 +100,100 @@ BOOSTER_T1_CFG = ArticulationCfg(
         ),
     },
 )
+
+BOOSTER_T1_LOW_GAIN_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ASSET_DIR}/booster/t1/t1.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=8, solver_velocity_iteration_count=4
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.72),
+        joint_pos={
+            # Head
+            "AAHead_yaw": 0.0,
+            "Head_pitch": 0.0,
+            # Arm
+            ".*_Shoulder_Pitch": 0.2,
+            "Left_Shoulder_Roll": -1.35,
+            "Right_Shoulder_Roll": 1.35,
+            ".*_Elbow_Pitch": 0.0,
+            "Left_Elbow_Yaw": -0.5,
+            "Right_Elbow_Yaw": 0.5,
+            # Waist
+            "Waist": 0.0,
+            # Leg
+            ".*_Hip_Pitch": -0.20,
+            ".*_Hip_Roll": 0.0,
+            ".*_Hip_Yaw": 0.0,
+            ".*_Knee_Pitch": 0.42,
+            ".*_Ankle_Pitch": -0.23,
+            ".*_Ankle_Roll": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "legs": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_Hip_Pitch",
+                ".*_Hip_Roll",
+                ".*_Hip_Yaw",
+                ".*_Knee_Pitch",
+                "Waist",
+            ],
+            effort_limit_sim={
+                ".*_Hip_Pitch": 45.0,
+                ".*_Hip_Roll": 30.0,
+                ".*_Hip_Yaw": 30.0,
+                ".*_Knee_Pitch": 60.0,
+                "Waist": 30.0,
+            },
+            velocity_limit_sim={
+                ".*_Hip_Pitch": 12.5,
+                ".*_Hip_Roll": 10.9,
+                ".*_Hip_Yaw": 10.9,
+                ".*_Knee_Pitch": 11.7,
+                "Waist": 10.88,
+            },
+            stiffness=100.0,
+            damping=5.0,
+            armature=0.01,
+        ),
+        "feet": ImplicitActuatorCfg(
+            joint_names_expr=[".*_Ankle_Pitch", ".*_Ankle_Roll"],
+            effort_limit_sim={".*_Ankle_Pitch": 24, ".*_Ankle_Roll": 15},
+            velocity_limit_sim={".*_Ankle_Pitch": 18.8, ".*_Ankle_Roll": 12.4},
+            stiffness=25.0,
+            damping=1.0,
+            armature=0.01,
+        ),
+        "arms": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_Shoulder_Pitch",
+                ".*_Shoulder_Roll",
+                ".*_Elbow_Pitch",
+                ".*_Elbow_Yaw",
+            ],
+            effort_limit_sim=18.0,
+            velocity_limit_sim=18.8,
+            stiffness=20.0,
+            damping=10.0,
+            armature=0.01,
+        ),
+    },
+)
 """Configuration for the Booster T1 Humanoid robot."""
 
 
--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/t1_ft/flat_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/t1_ft/flat_env_cfg.py
@@ -1,6 +1,6 @@
 from isaaclab.utils import configclass
 
-from whole_body_tracking.robots.t1 import T1_ACTION_SCALE, BOOSTER_T1_CFG
+from whole_body_tracking.robots.t1 import T1_ACTION_SCALE, BOOSTER_T1_LOW_GAIN_CFG
 from whole_body_tracking.tasks.tracking.config.g1.agents.rsl_rl_ppo_cfg import LOW_FREQ_SCALE
 from whole_body_tracking.tasks.tracking.tracking_ft_env_cfg import TrackingFTEnvCfg
 
@@ -10,7 +10,7 @@ class T1FTEnvCfg(TrackingFTEnvCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.scene.robot = BOOSTER_T1_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        self.scene.robot = BOOSTER_T1_LOW_GAIN_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.actions.joint_pos.scale = T1_ACTION_SCALE
         self.commands.motion.anchor_body_name = "Trunk"
         self.commands.motion.body_names = [
