7829378096406b9f1ba739384f54dcd4a6ebb2b7
SavvyProp <ludwigtaycheeying@gmail.com>
2025-11-02 23:15:35 -0500

    cartwheel

--- retarget.sh
+++ retarget.sh
@@ -1,3 +1,3 @@
-python scripts/csv_to_npz.py --input_file data/accad_crawl2.npz --input_fps 30 --output_name accad_crawl2 --headless
+python scripts/csv_to_npz.py --input_file data/accad_cartwheel.npz --input_fps 30 --output_name accad_cartwheel --headless
 
-python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/accad_crawl2
+python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/accad_cartwheel
--- train_ft.sh
+++ train_ft.sh
@@ -1,3 +1,3 @@
 python scripts/rsl_rl/train.py --task=Tracking-FT-T1-v0 \
---registry_name ludwigtaycheeying-purdue-university-org/wandb-registry-motions/accad_crawl2 \
---headless --logger wandb --log_project_name t1ft_fb --run_name accad_crawl2
+--registry_name ludwigtaycheeying-purdue-university-org/wandb-registry-motions/accad_cartwheel \
+--headless --logger wandb --log_project_name t1ft_fb --run_name accad_cartwheel
