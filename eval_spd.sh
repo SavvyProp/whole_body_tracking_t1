3b4c45eb7068d1f196e1527706bd6cb2102291c3
SavvyProp <ludwigtaycheeying@gmail.com>
2025-11-04 10:43:05 -0500

    lower tracking rewards

--- eval_spd.sh
+++ eval_spd.sh
@@ -1,1 +1,1 @@
-python scripts/rsl_rl/eval_spd.py --task=Tracking-Flat-T1-v0 --num_envs=64 --wandb_path=ludwigtaycheeying-purdue-university/t1_flat/d5lf7doq --headless
+python scripts/rsl_rl/eval_spd.py --task=Tracking-Flat-T1-v0 --num_envs=64 --wandb_path=ludwigtaycheeying-purdue-university/t1_flat/croso8ba --headless
