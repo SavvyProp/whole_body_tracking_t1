d862c421b1174fc5c2a17c4993c80de9b49635b2
SavvyProp <ludwigtaycheeying@gmail.com>
2025-12-23 18:40:46 -0500

    added back anchor

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python scripts/rsl_rl/play.py --task=Tracking-Flat-T1-v0 --num_envs=2 --wandb_path=ludwigtaycheeying-purdue-university/t1_flat_bmarm/e5qyjbtb
+python scripts/rsl_rl/play.py --task=Tracking-Flat-T1-v0 --num_envs=2 --wandb_path=ludwigtaycheeying-purdue-university/t1_flat_bmarm/c15qz1c6
