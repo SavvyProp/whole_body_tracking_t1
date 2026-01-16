850d47abd3a33abfb644ea5a2af5679f28f86199
SavvyProp <ludwigtaycheeying@gmail.com>
2026-01-16 16:02:56 -0500

    much lower gains

--- train_ft.sh
+++ train_ft.sh
@@ -1,3 +1,3 @@
 python scripts/rsl_rl/train.py --task=Tracking-FT-T1-v0 \
---registry_name ludwigtaycheeying-purdue-university-org/wandb-registry-motions/CMU_40_05 \
---headless --logger wandb --log_project_name t1_lcc_29 --run_name CMU_40_05
+--registry_name ludwigtaycheeying-purdue-university-org/wandb-registry-motions/CMU_36_09 \
+--headless --logger wandb --log_project_name t1_lcc_29 --run_name CMU_36_09
