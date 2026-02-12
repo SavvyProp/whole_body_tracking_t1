f56b2c0ee0425373b894f4b49c53f293edcdc986
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-11 23:23:07 -0500

    new motions

--- retarget.sh
+++ retarget.sh
@@ -1,3 +1,3 @@
-python scripts/csv_to_npz.py --input_file data/lafan_sidestep_trans.npz --input_fps 30 --output_name lafan_sidestep_trans --headless
+python scripts/csv_to_npz.py --input_file data/HDM_WT_trans2.npz --input_fps 30 --output_name HDM_WT_trans2 --headless
 
-python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/lafan_sidestep_trans
+python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/HDM_WT_trans2
