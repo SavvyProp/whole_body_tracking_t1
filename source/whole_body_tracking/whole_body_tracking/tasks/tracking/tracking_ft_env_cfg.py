e279860605d4191ddc35ae5dff6b4f3337df080c
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-23 20:36:34 -0500

    a

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_ft_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_ft_env_cfg.py
@@ -412,12 +412,12 @@ class RewardsCfg:
     motion_body_lin_vel = RewTerm(
         func=mdp.motion_global_body_linear_velocity_error_exp,
         weight=1.5,
-        params={"command_name": "motion", "std": 0.25},
+        params={"command_name": "motion", "std": 0.6},
     )
     motion_body_ang_vel = RewTerm(
         func=mdp.motion_global_body_angular_velocity_error_exp,
         weight=1.5,
-        params={"command_name": "motion", "std": 3.14/1.5},
+        params={"command_name": "motion", "std": 3.14/1.0},
     )
     action_rate_l2 = RewTerm(func=mdp.ft_action_rate_l2, weight=-8e-2)
     joint_limit = RewTerm(
