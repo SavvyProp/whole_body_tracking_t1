3654cb4cd9245c7e8a13fc20e2bf6958a708cc9e
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-25 23:50:18 -0500

    balance

--- retarget.sh
+++ retarget.sh
@@ -1,3 +1,3 @@
-python scripts/csv_to_npz.py --input_file data/t1_23_hk_trans.npz --input_fps 30 --output_name t1_23_hk_trans --headless
+python scripts/csv_to_npz.py --input_file data/t1_23_balance_trans.npz --input_fps 30 --output_name t1_23_balance_trans --headless
 
-python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/t1_23_hk_trans
+python scripts/replay_npz.py --registry_name=ludwigtaycheeying-purdue-university-org/wandb-registry-motions/t1_23_balance_trans
